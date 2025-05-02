d9fc65502b0cdcc001f687b5156aa1e75b4498ea
NomaTyx <jjziebart99@gmail.com>
2025-05-01 23:32:11 -0700

    added ability for player to dash in the direction the camera is facing

--- Assets/Scripts/CharacterMovement/PlayerController.cs
+++ Assets/Scripts/CharacterMovement/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
     [field: SerializeField] protected CursorLockMode CursorMode { get; set; } = CursorLockMode.Locked;
     // make character look in Camera direction instead of MoveDirection
     [field: SerializeField] protected bool LookInCameraDirection { get; set; }
+    [field: SerializeField] private float DashForce = 100;
+    [field: SerializeField] private float LaunchForce = 100;
+    [SerializeField] private Transform cameraTransform;
 
     [field: Header("Componenents")]
 
@@ -21,6 +24,14 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Added Force.");
         rb.AddForce(new Vector3(10, 100, 10), ForceMode.Impulse);
+    }
+
+    public void OnDash()
+    {
+        Vector3 playerVelocity = rb.linearVelocity;
+        rb.linearVelocity = Vector3.zero;
+        //possibly change this to just set the velocity equal to the value, who knows
+        rb.AddForce(new Vector3(cameraTransform.forward.x, rb.linearVelocity.y, cameraTransform.forward.z) * DashForce, ForceMode.Impulse);
     }
 
     protected virtual void Update()
