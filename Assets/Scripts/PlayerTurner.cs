651da489db1ef157259adabf68fb0101c692fcd7
NomaTyx <jjziebart99@gmail.com>
2025-05-01 22:58:33 -0700

    cleaned up unused code and made starting platform bigger

--- Assets/Scripts/PlayerTurner.cs
+++ Assets/Scripts/PlayerTurner.cs
@@ -6,12 +6,10 @@ public class PlayerTurner : MonoBehaviour
     //this value will be disabled in certain cases
     public bool TurnPlayer = true;
 
-    [SerializeField] private int _rotationSpeed = 5;
     [SerializeField] private int _yWeight = 3;
 
     private Rigidbody rb;
     private Quaternion rotation;
-    float time = 0;
 
     private void Start()
     {
