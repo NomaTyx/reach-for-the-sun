e9f7980957c7dd17966720dd1431544ef1becb28
NomaTyx <jjziebart99@gmail.com>
2025-05-05 01:23:00 -0700

    finished work on health for today

--- Assets/Scripts/Characters/Health/DamageInfo.cs
+++ Assets/Scripts/Characters/Health/DamageInfo.cs
@@ -4,9 +4,9 @@ public class DamageInfo : MonoBehaviour
 {
     public float Amount { get; private set; }
     public GameObject Instigator { get; private set; }
-    public Health Target { get; private set; }
+    public GameObject Target { get; private set; }
 
-    public DamageInfo(float amount, GameObject instigator, Health target)
+    public DamageInfo(float amount, GameObject instigator, GameObject target)
     {
         Target = target;
         Amount = amount;
--- Assets/Scripts/Characters/Health/Health.cs
+++ Assets/Scripts/Characters/Health/Health.cs
@@ -4,9 +4,39 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float _maxHealth = 1;
     [SerializeField] private float _currentHealth = 1;
+    [SerializeField] private float _damageTestPercent = 10;
 
+    public float Max => _maxHealth;
+    public float Current => _currentHealth;
+    public float Percentage => _currentHealth / _maxHealth;
+    public float Missing => _maxHealth - _currentHealth;
+    public bool IsAlive => _currentHealth > 0;
+
+    public Event OnDamage;
+    public Event OnDeath;
+
+    /// <summary>
+    /// damage method
+    /// </summary>
+    /// <param name="info"></param>
     public void Damage(DamageInfo info)
     {
-        currentHealth 
+        _currentHealth -= info.Amount;
+
+    }
+
+    /// <summary>
+    /// temporary heal method
+    /// </summary>
+    public void Heal()
+    {
+
+    }
+
+    [ContextMenu("Damage test 10%")]
+    public void DamageTest()
+    {
+        DamageInfo info = new DamageInfo(_maxHealth * (_damageTestPercent / 100), gameObject, gameObject);
+        Damage(info);
     }
 }
