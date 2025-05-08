5961b17022b470fcfc12ec95a38ebc4e4e49e455
NomaTyx <jjziebart99@gmail.com>
2025-05-08 00:06:21 -0700

    created coroutine for enemy and made placeholder attack method. currently enemy just attacks the player on a cooldown

--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -1,13 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     private Health _health;
+    [SerializeField] private float tempVar_AttackCooldown = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _health = GetComponent<Health>();
         _health.OnDamage += DamageBehavior;
+
+        StartCoroutine(AggressiveState());
     }
 
     private void OnDestroy()
@@ -27,5 +31,29 @@ public class EnemyController : MonoBehaviour
     void DamageBehavior()
     {
         Debug.Log("penis");
+    }
+
+    void Attack()
+    {
+        Debug.Log("Bam! Attacked");
+    }
+
+    //i might end up doing some crap with an enemy AI manager that sends instructions to enemies so that i can make them more coordinated
+    //currently they operate independently tho
+    private IEnumerator AggressiveState()
+    {
+        float AttackTime = 0;
+        //this is (currently!) the default AI behavior, so we're just looping forever
+        while (true)
+        {
+            Debug.Log("coroutine is going");
+            AttackTime += Time.deltaTime;
+            if(AttackTime > tempVar_AttackCooldown)
+            {
+                Attack();
+                AttackTime = 0;
+            }
+            yield return null;
+        }
     }
 }
