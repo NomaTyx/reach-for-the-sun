6a02a7cf396ba29e2617bab9cee22295caf2d8cb
NomaTyx <jjziebart99@gmail.com>
2025-05-11 01:32:42 -0700

    fixed camera dashing issue!! Code is a little bit cleaner I think.

---
+++ Assets/Scripts/Characters/Player/FollowPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+
+    // this is a script specifically made so that i can have an object that moves with the player but doesn't rotate with it.
+    private void Update()
+    {
+        transform.position = playerTransform.position;
+    }
+}
