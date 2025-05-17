39d63c01b593c34e556cff5f304bc476dff9bf6a
NomaTyx <jjziebart99@gmail.com>
2025-05-17 00:33:25 -0700

    made projectile hopefully rotate to follow target. had plans tonight so couldn't rlly do more.

--- Assets/Scripts/Characters/Enemies/Projectile.cs
+++ Assets/Scripts/Characters/Enemies/Projectile.cs
@@ -5,6 +5,7 @@ public class Projectile : MonoBehaviour
     private GameObject _target;
     private float _damage;
     private float _speed;
+    private Rigidbody _rb;
 
     public Projectile(GameObject target, float damage, float speed)
     {
@@ -13,9 +14,20 @@ public class Projectile : MonoBehaviour
         _speed = speed;
     }
 
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         //var targetRotation = Quaternion.LookRotation()
+        Vector3 targetDirection = _target.transform.position - transform.position;
+        targetDirection.y = 0;
+        targetDirection.Normalize();
+        Vector3 target = Vector3.RotateTowards(transform.forward, targetDirection, Time.fixedDeltaTime * 0.3f, 0);
+        transform.rotation = Quaternion.LookRotation(target);
+        _rb.linearVelocity = transform.forward * _speed;
     }
 
     //
