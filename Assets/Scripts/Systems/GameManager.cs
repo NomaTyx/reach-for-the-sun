4461b99408812a8cfd17841d5fa78d16fa906821
NomaTyx <jjziebart99@gmail.com>
2025-05-21 23:08:49 -0700

    renamed gamestatemanager

---
+++ Assets/Scripts/Systems/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /*TODO implement states:
+     - Dead state
+     - gameplay state
+     - UI state
+    */
+}
