3d6cb3372a45bc34f9ae4e0339a001fbf3cf78c4
NomaTyx <jjziebart99@gmail.com>
2025-05-23 00:36:22 -0700

    started game state management-- see how well core mechanics gel together. See what needs to be added. DO LEVEL DESIGN

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
     /*TODO implement states:
      - Dead state
      - gameplay state
      - UI state
     */
+
+    public GameState State;
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("There's already an instance of this singleton!");
+            return;
+        }
+
+        Instance = this;
+    }
 }
