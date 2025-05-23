0e8fb6238f8311b4ff733b2b2ff4a35b54387379
NomaTyx <jjziebart99@gmail.com>
2025-05-22 23:26:49 -0700

    made gamestate script

---
+++ Assets/Scripts/Systems/GameState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+//this class is mostly for practice making game states, i don't think this is strictly necessary
+//it makes the game scalable tho
+public class GameState
+{
+    
+}
