d8b1d62087afdf152079597e7fb352cf9a4a679d
NomaTyx <jjziebart99@gmail.com>
2025-05-24 22:49:13 -0700

    started trying to get parrying to work

--- Assets/Scripts/Characters/Enemies/RocketLauncher.cs
+++ Assets/Scripts/Characters/Enemies/RocketLauncher.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class RocketLauncher : EnemyWeapon
 {
     [SerializeField] private GameObject _projectile;
-    [SerializeField] private float _projectileDamage;
-    [SerializeField] private float _projectileSpeed;
+    [SerializeField] private float _projectileDamage = 1;
+    [SerializeField] private float _projectileSpeed = 15;
 
     protected override void Attack(GameObject target, GameObject instigator)
     {
         Rocket proj = Instantiate(_projectile, transform.position, Quaternion.identity).GetComponent<Rocket>();
-        proj.Init(target, _projectileDamage, _projectileSpeed);
+        proj.Init(target, new DamageInfo(_projectileDamage, instigator, target), _projectileSpeed);
     }
 }
