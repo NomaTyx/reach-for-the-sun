6c72af6b391553380819f9d30537ad5fb810be7a
NomaTyx <jjziebart99@gmail.com>
2025-05-27 00:01:11 -0700

    added logic for an "activity indicator" (the dash icon on the HUD will change color when you are currently dashing)

--- Assets/Scripts/UI/CooldownBar.cs
+++ Assets/Scripts/UI/CooldownBar.cs
@@ -6,12 +6,20 @@ public class CooldownBar : MonoBehaviour
 {
     //i don't want to have to serialize this, but getcomponentinchildren gets the component from the parent as well, so this is just the best i can do.
     [SerializeField] private Image _fillBar;
+    [SerializeField] private Image _activityIcon; //i really don't have a good name for this sorry whoever is reading this lol (unless you're julia, fuck you)
 
     private float _cooldownStartTime;
     private float _cooldownDuration;
 
+    public void ShowIfAbilityActive()
+    {
+        Debug.Log("abilityactive is called");
+        _activityIcon.enabled = !_activityIcon.enabled;
+    }
+
     public void StartCooldown(float cooldown)
     {
+        ShowIfAbilityActive();
         _cooldownStartTime = Time.time;
         _cooldownDuration = cooldown;
         _fillBar.fillAmount = 1;
