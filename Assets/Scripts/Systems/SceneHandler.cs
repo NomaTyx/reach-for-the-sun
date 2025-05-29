bde946e924763b6848d0dd14bd8ef51836e98b7c
NomaTyx <jjziebart99@gmail.com>
2025-05-29 01:03:55 -0700

    made title screen lead to test level 1

---
+++ Assets/Scripts/Systems/SceneHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    private static SceneHandler _instance;
+
+    public static SceneHandler Instance => _instance;
+
+    [SerializeField] private Animator _transition;
+    [SerializeField] private float _transitionTime = 1;
+
+    private string _sceneName;
+
+    private void Start()
+    {
+        if (_instance != null)
+        {
+            Debug.Log("There already exists an instance of this singleton!");
+            Destroy(_instance);
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
