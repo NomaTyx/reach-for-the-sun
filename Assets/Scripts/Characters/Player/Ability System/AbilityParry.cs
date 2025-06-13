d33aeabbf2a32714613a470efd95b2a716893b8d
NomaTyx <jjziebart99@gmail.com>
2025-06-13 00:41:02 -0700

    made parry ability. Now i just need to clean up references to the previous ability system

---
+++ Assets/Scripts/Characters/Player/Ability System/AbilityParry.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class AbilityParry : AbilityBase
+{
+    private float _parryCooldown = 2f;
+    private float _parryRange = 5f
+
+    public override void Init()
+    {
+        base.Init();
+        AbilityName = "Parry";
+        //hardcoding cooldowns and durations for now, until i can decide where to store them
+        CooldownDuration = _parryCooldown;
+        EffectDuration = 0;
+
+        cooldownWFS = new WaitForSecondsRealtime(CooldownDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check only the projectile layer
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _parryRange);
+
+        foreach (Collider c in colliders)
+        {
+            c.TryGetComponent<Rocket>(out Rocket rocket);
+
+            if (rocket != null)
+            {
+                rocket.ParryProjectile();
+            }
+        }
+    }
+}
