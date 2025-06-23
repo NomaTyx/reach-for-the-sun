ff25798cbd239d2f609e0273b15beec097488ba7
NomaTyx <jjziebart99@gmail.com>
2025-06-22 21:58:35 -0700

    added counter for bullets

--- Assets/Scripts/EnemyAIManager.cs
+++ Assets/Scripts/EnemyAIManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class EnemyAIManager : MonoBehaviour
 {
     private List<EnemyController> _enemiesInScene = new List<EnemyController>();
+    private int _numOfActiveBullets = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,15 @@ public class EnemyAIManager : MonoBehaviour
         }
 
         foreach (EnemyController go in _enemiesInScene) Debug.Log(go.gameObject.name);
+    }
+
+    public void RegisterSpawnedBullet()
+    {
+        _numOfActiveBullets++;
+    }
+
+    public void RegisterDestroyedBullet()
+    {
+        _numOfActiveBullets--;
     }
 }
