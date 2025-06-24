17a860db6b2834dfc3efc2d23d8362b92231fde1
NomaTyx <jjziebart99@gmail.com>
2025-06-23 21:44:31 -0700

    slowly building out enemy ai script

--- Assets/Scripts/EnemyAIManager.cs
+++ Assets/Scripts/EnemyAIManager.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 
 public class EnemyAIManager : MonoBehaviour
 {
+    public static EnemyAIManager Instance;
     private List<EnemyController> _enemiesInScene = new List<EnemyController>();
     private int _numOfActiveBullets = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (Instance != null)
+        {
+            Debug.Log("There's already an instance of this singleton!");
+            return;
+        }
+
+        Instance = this;
+
         foreach (EnemyController enemy in FindObjectsByType<EnemyController>(FindObjectsSortMode.None)) {
             _enemiesInScene.Add(enemy);
         }
-
-        foreach (EnemyController go in _enemiesInScene) Debug.Log(go.gameObject.name);
     }
 
     public void RegisterSpawnedBullet()
@@ -24,5 +31,10 @@ public class EnemyAIManager : MonoBehaviour
     public void RegisterDestroyedBullet()
     {
         _numOfActiveBullets--;
+    }
+
+    public void RegisterSpawnedEnemy(GameObject enemy) //passing in gameobject in case i need to do some sort of logic
+    {
+        _enemiesInScene.Add(enemy.GetComponent<EnemyController>());
     }
 }
