70e1f9aa8da2b8d361ad97179901b1e158e8b80a
NomaTyx <jjziebart99@gmail.com>
2025-06-30 23:56:05 -0700

    started on the zip file

--- Assets/Scripts/Player/Ability System/AbilityZip.cs
+++ Assets/Scripts/Player/Ability System/AbilityZip.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class AbilityZip : MonoBehaviour
+public class AbilityZip : AbilityBase
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
